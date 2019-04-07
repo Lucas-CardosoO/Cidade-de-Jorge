462a73b050b7300ed7bcc64ba59065f38128e8d5
Kacio <kacio@10-48-40-17.0101.ufpe.br.iosda.org>
2019-04-07 06:43:16 -0300

    3dJorge

--- SalveJorge/SalveJorge/City.swift
+++ SalveJorge/SalveJorge/City.swift
@@ -13,6 +13,7 @@ class City:NSObject{
     var empresarialRating:Int
     var recursos:Int
     var arrecadamento:Int
+    var delegate:CityComunication?
     init(populationRating:Int , empresarialRating:Int , recursos:Int , arrecadamento:Int){
         self.populationRating = populationRating
         self.empresarialRating = empresarialRating
@@ -21,5 +22,79 @@ class City:NSObject{
     }
     // recebe a acao tomada e aplica isso a cidade
     func cityNextRound(acao:(AcoesTomadas,AcoesTomadas)){
+        
+        self.delegate?.dead()
+    }
+    func cityEfect(action:AcoesTomadas){
+        switch action {
+        case .parque :
+            self.arrecadamento -= 100
+            self.recursos -= 200
+            self.empresarialRating -= 15
+            self.populationRating += 30
+            break
+        case .hospital:
+            self.arrecadamento -= 100
+            self.recursos -= 200
+            self.populationRating += 30
+            self.empresarialRating -= 15
+            break
+        case .escolaPublicaPeriferia:
+            self.arrecadamento -= 100
+            self.recursos -= 100
+            self.populationRating += 15
+            break
+        case .escolaPublicaAreaCentral:
+            self.arrecadamento -= 100
+            self.recursos -= 200
+            self.populationRating += 30
+            self.empresarialRating -= 15
+            break
+        case .financiarHabitacaoAreaNobre:
+            self.recursos -= 100
+            self.populationRating += 30
+            self.empresarialRating -= 30
+            break
+        case .financiarHabitacaoPerifeiria:
+            self.recursos -= 100
+            self.populationRating += 15
+            break
+        case .fabricaAreaLivre:
+            self.recursos -= 200
+            self.arrecadamento += 200
+            self.empresarialRating += 30
+            break
+        case .fabricaAreaOcupada:
+            self.recursos -= 100
+            self.arrecadamento += 200
+            self.populationRating -= 30
+            self.empresarialRating += 15
+            break
+        case .arranhaCeuDeLuxoNobre:
+            self.recursos += 100
+            self.arrecadamento += 200
+            self.populationRating -= 15
+            self.empresarialRating += 30
+            break
+        case .arranhaCeuDeLuxoAreaPreriferia:
+            self.arrecadamento += 100
+            self.populationRating -= 15
+            self.empresarialRating += 15
+            break
+        case .shoppingAreaLivre:
+            self.arrecadamento += 100
+            self.recursos -= 200
+            self.populationRating += 15
+            self.empresarialRating += 30
+            break
+        case .shopingAreaOcupada:
+            self.arrecadamento += 100
+            self.populationRating -= 30
+            self.empresarialRating += 30
+            break
+        default:
+            self.populationRating -= 15
+            break
+        }
     }
 }
